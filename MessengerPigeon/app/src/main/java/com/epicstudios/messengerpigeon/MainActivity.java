766d421931c0e8d85aa63eb428d3593c681cd37b
Suzy <35238835+Chewypuppielover@users.noreply.github.com>
2018-03-14 10:42:16 -0600

    fixed presenter implementation error

--- MessengerPigeon/app/src/main/java/com/epicstudios/messengerpigeon/MainActivity.java
+++ MessengerPigeon/app/src/main/java/com/epicstudios/messengerpigeon/MainActivity.java
@@ -26,6 +26,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.startService(new Intent(this, QuickResponseService.class));
-
+        presenter = new Presenter(new LinkedList<Conversation>());
         conversationList.add("Only for now");
         conversations = (ListView) findViewById(R.id.conversations);
         arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, presenter.getConversations());
