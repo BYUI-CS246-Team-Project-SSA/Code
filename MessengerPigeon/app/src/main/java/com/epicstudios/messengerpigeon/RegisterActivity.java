9fc471740b28c328c98a181d906f2dd8b0024827
ashloo141 <loo16001@byui.edu>
2018-03-19 11:07:20 -0600

    Adds user info to database when a new account is created.

--- MessengerPigeon/app/src/main/java/com/epicstudios/messengerpigeon/RegisterActivity.java
+++ MessengerPigeon/app/src/main/java/com/epicstudios/messengerpigeon/RegisterActivity.java
@@ -16,10 +16,13 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private FirebaseAuth auth;
+    private DatabaseReference storeUserDataRef;
     private ProgressDialog loadBar;
 
     private EditText userName;
@@ -52,7 +55,7 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
 
-    private void registerAccount(String name, String email, String pswd)
+    private void registerAccount(final String name, String email, String pswd)
     {
         if (TextUtils.isEmpty(name))
         {
@@ -87,11 +90,26 @@ public class RegisterActivity extends AppCompatActivity {
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful())
                     {
-                        Intent mainIntent = new Intent(RegisterActivity.this,
-                                                                     LoginActivity.class);
-                        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                        startActivity(mainIntent);
-                        finish();
+                        String currentUid = auth.getCurrentUser().getUid();
+                        storeUserDataRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUid);
+
+                        storeUserDataRef.child("user_name").setValue(name);
+                        storeUserDataRef.child("user_status").setValue("Hey, I'm using MessengerPigeon!!");
+                        storeUserDataRef.child("user_image").setValue("default_profile");
+                        storeUserDataRef.child("user_thumb_image").setValue("default_image")
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+
+                                        if (task.isSuccessful()) {
+                                            Intent mainIntent = new Intent(RegisterActivity.this,
+                                                    LoginActivity.class);
+                                            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                            startActivity(mainIntent);
+                                            finish();
+                                        }
+                                    }
+                                });
                     }
                     else
                     {
