10373751c6d452b7d199ccf514ff0fa42e1b9135
Suzy <35238835+Chewypuppielover@users.noreply.github.com>
2018-04-05 23:03:39 -0600

    now grab numbers and SMS send works

--- MessengerPigeon/app/src/main/java/com/epicstudios/messengerpigeon/DisplayGroupActivity.java
+++ MessengerPigeon/app/src/main/java/com/epicstudios/messengerpigeon/DisplayGroupActivity.java
@@ -1,27 +1,16 @@
 package com.epicstudios.messengerpigeon;
 
-import android.content.ContentResolver;
-import android.content.Context;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.Pair;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class DisplayGroupActivity extends AppCompatActivity {
@@ -30,51 +19,33 @@ public class DisplayGroupActivity extends AppCompatActivity {
     private ListView people;
     private contactAdapter contactAdapter;
     private Presenter presenter;
-    //private EditText input;
+    private EditText input;
+    ArrayList<Pair<Long, String>> listPeople;
+
+    public DisplayGroupActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
-        //input = (EditText) findViewById(R.id.input);*/
+        input = (EditText) findViewById(R.id.input);
 
         Bundle extras = getIntent().getExtras();
         List<String> temp = extras.getStringArrayList("Groups");
         List<String> tempIDs = extras.getStringArrayList("GroupIDs");
         presenter = new Presenter(this, temp, tempIDs);
         int index = extras.getInt("index");
-        final ArrayList<Pair<Long, String>> listPeople = presenter.getGroup(index);
+        listPeople = presenter.getGroup(index);
         Log.d(TAG, "listPeople: "+ listPeople.toString());
 
         people = (ListView) findViewById(R.id.peoples);
-        contactAdapter = new contactAdapter(this, R.layout.contact_display, listPeople);
-        //contactAdapter = new contactAdapter(this, android.R.layout.select_dialog_multichoice, listPeople);
+        contactAdapter = new contactAdapter(this, R.layout.contact_display, listPeople, presenter.getPhotos());
         people.setAdapter(contactAdapter);
-
-        ArrayAdapter arrayAdapter;
-        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.select_dialog_multichoice, listPeople);
-        //arrayAdapter = new ArrayAdapter<>(this, R.layout.contact_display, listPeople);
-        //people.setAdapter(arrayAdapter);
 
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
             //getPermissionToReadContacts(); This should be in a fragment
         }
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
-            //getPermissionToReadSMS(); This should be in a fragment
-        }
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        /*active = true;
-        inst = this;*/
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        //active = false;
     }
 
     /**
@@ -87,17 +58,13 @@ public class DisplayGroupActivity extends AppCompatActivity {
                 != PackageManager.PERMISSION_GRANTED) {
             //getPermissionToReadSMS(); This should be in a fragment activity
         }
+        //TODO presenter.addConversation();
+        presenter.addConversation(listPeople);
+
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.SEND_SMS)//TODO mms
                 != PackageManager.PERMISSION_GRANTED) {
             //getPermissionToReadSMS(); This should be in a fragment activity
         }
-        //TODO presenter.addConversation();
-    }
 
-
-    //TODO check if necessary
-    public void updateList(final String smsMessage) {
-        //contactAdapter.insert(smsMessage, 0);
-        //contactAdapter.notifyDataSetChanged();
     }
 }
--- MessengerPigeon/app/src/main/java/com/epicstudios/messengerpigeon/Presenter.java
+++ MessengerPigeon/app/src/main/java/com/epicstudios/messengerpigeon/Presenter.java
@@ -4,8 +4,11 @@ import android.app.PendingIntent;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcel;
 import android.provider.ContactsContract;
 import android.telephony.SmsManager;
 import android.util.Log;
@@ -14,78 +17,65 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.Set;
 
-/*
-import com.klinker.android.logger.Log;
-import com.klinker.android.logger.OnLogListener;
-import com.klinker.android.send_message.ApnUtils;
-import com.klinker.android.send_message.BroadcastUtils;
-import com.klinker.android.send_message.Message;
-import com.klinker.android.send_message.Transaction;
-import com.klinker.android.send_message.Utils;
-import com.klinker.android.send_message.Settings;
-
-import android.graphics.BitmapFactory;
-*/
 public class Presenter {
     private static final String TAG = "Presenter:";
     private final Context context;
     private List<String> groups = new ArrayList<>();
     private List<String> groupIDs = new ArrayList<>();
+    private HashMap<Long, String> ContactPhotos;
     private ContentResolver cr;
     private SmsManager smsManager;
-    //private String phonenum = "5053990094";
-    //TO DO private Database =
 
     /**
      * Initiation of the presenter passing in the context of the activity
      * @param cont context of the Activity creating presenter
      */
-    public Presenter(Context cont) {
+    Presenter(Context cont) {
         Log.i(TAG, "Presenter created.");
         context = cont;
         cr = context.getContentResolver();
+
         /****************Tests*********************
         groups.add("Group One");
         groups.add("Group Two");
         /*************************************/
+
         retrieveGroups();
     }
 
     /**
      * Initiation of the presenter passing in the context of the activity, and group information
      * @param cont context of the Activity creating presenter
-     * @param grouptitles list of group titles
-     * @param groupids list of group ids
+     * @param groupTitles list of group titles
+     * @param groupIds list of group ids
      */
-    public Presenter(Context cont, List<String> grouptitles, List<String> groupids){
+    Presenter(Context cont, List<String> groupTitles, List<String> groupIds){
         Log.i(TAG, "Presenter created.");
         context = cont;
         cr = context.getContentResolver();
-        groups = grouptitles;
-        groupIDs = groupids;
+        groups = groupTitles;
+        groupIDs = groupIds;
     }
-
+    /**
     public void copyPresenter(Presenter toBe){
         this.groups = toBe.getGroups();
         this.groupIDs = toBe.getGroupIDs();
-    }
+    }*/
 
     /**
      * returns list of all the groups saved on your phone(/google) by name
      * @return list of all the groups saved on your phone(/google) by String name
      */
-    public List<String> getGroups(){ return groups; }
+    List<String> getGroups(){ return groups; }
 
     /**
      * returns list of all the groups ID's saved on your phone(/google) by name
      * @return list of all the groups ID's saved on your phone(/google) by String name
      */
-    public List<String> getGroupIDs(){ return groupIDs; }
+    List<String> getGroupIDs(){ return groupIDs; }
 
     /**
      * makes list of all the groups saved on your phone(/google) by String name
@@ -93,29 +83,33 @@ public class Presenter {
     private void retrieveGroups() { //TODO DONE
         Uri uri = ContactsContract.Groups.CONTENT_SUMMARY_URI;
         String[] clms = new String[]{ContactsContract.Groups.TITLE, ContactsContract.Groups._ID,
-                ContactsContract.Groups.SUMMARY_COUNT,
+                //ContactsContract.Groups.SUMMARY_COUNT,
                 ContactsContract.Groups.SUMMARY_WITH_PHONES
         };
         String selection = ContactsContract.Groups.DELETED + "=0";// and " + ContactsContract.Groups.GROUP_VISIBLE + "=1";
         Cursor gcursor = cr.query(uri, clms, selection, null, null);
+
         if (gcursor != null) {
-            if(gcursor.getCount() < 1) {
+
+            final int titlePos = gcursor.getColumnIndex(ContactsContract.Groups.TITLE);
+            final int idPos = gcursor.getColumnIndex(ContactsContract.Groups._ID);
+            //final int countPos = gcursor.getColumnIndex(ContactsContract.Groups.SUMMARY_COUNT);
+            final int phoneCountPos = gcursor.getColumnIndex(ContactsContract.Groups.SUMMARY_WITH_PHONES);
+
+            if (gcursor.getCount() < 1) {
                 groups.add("Found no Groups");
-            }
-            else{
-                while(gcursor.moveToNext()){
-                    String title = gcursor.getString(0);
-                    if(!(gcursor.getString(3).equals("0"))) {
+                groupIDs.add("NA");
+            } else {
+                while (gcursor.moveToNext()) {
+                    String title = gcursor.getString(titlePos);
+                    if (!(gcursor.getString(phoneCountPos).equals("0"))) {
                         groups.add(title);
-                        groupIDs.add(gcursor.getString(1));
+                        groupIDs.add(gcursor.getString(idPos));
                     }
-                    //Log.d(TAG+":groups", title+" has "+gcursor.getString(2)+" contacts");
-                    //Log.d(TAG+":groups", title+" has "+gcursor.getString(3)+" contacts with phone numbers");
                 }
-                Log.d(TAG+"groups", "groups retrieved");
+                Log.d(TAG + "groups", "groups retrieved");
             }
-        }
-        else {
+        } else {
             //TO DO Something went wrong, except it crashes if anything goes wrong here
         }
         if (gcursor != null && !gcursor.isClosed()) {
@@ -128,58 +122,55 @@ public class Presenter {
      * @param index the index of the group that is being shown
      * @return the list of contacts, with phone numbers, in group
      */
-    public ArrayList<Pair<Long, String>> getGroup(int index){
+    ArrayList<Pair<Long, String>> getGroup(int index){ //TODO DONE
         String title = groups.get(index);
         String id = groupIDs.get(index);
         Log.d(TAG, "groupID = "+id);
-        ArrayList<Pair<Long, String>> people = new ArrayList<Pair<Long, String>>();
 
-        final String GROUPMEMBERSHIP = "'"+ContactsContract.CommonDataKinds.GroupMembership.CONTENT_ITEM_TYPE+"'";
+        ArrayList<Pair<Long, String>> people = new ArrayList<Pair<Long, String>>();
+        ContactPhotos = new HashMap();
+
+        //final String PHOTO = ContactsContract.CommonDataKinds.Photo.CONTENT_ITEM_TYPE;
+        final String GROUPMEMBERSHIP = ContactsContract.CommonDataKinds.GroupMembership.CONTENT_ITEM_TYPE;
 
         Uri data = ContactsContract.Data.CONTENT_URI;
-        String[] clmsD = new String[]{
+        String[] clms = new String[]{
                 ContactsContract.Data.DISPLAY_NAME,
-                ContactsContract.Data.IN_VISIBLE_GROUP,
                 ContactsContract.Data.CONTACT_ID,
-                ContactsContract.Contacts._ID,
-                ContactsContract.Data.LOOKUP_KEY,
-                ContactsContract.Data.MIMETYPE,
-                ContactsContract.Data.DATA1,
+                ContactsContract.Data.PHOTO_URI,
         };
-
-        String selection = ContactsContract.Data.HAS_PHONE_NUMBER+"=1 AND "+
-                ContactsContract.Data.MIMETYPE+"="+GROUPMEMBERSHIP+" AND "+
-                ContactsContract.Data.DATA1+"=?";
         String[] selectionArgs = {id};
-
-        Cursor cursor = cr.query(data, clmsD, selection, selectionArgs, null);
-
-        final int idPos = cursor.getColumnIndex(ContactsContract.Data.CONTACT_ID);
-        final int namePos = cursor.getColumnIndex(ContactsContract.Data.DISPLAY_NAME);
-        final int dataPos = cursor.getColumnIndex(ContactsContract.Data.DATA1);
-        final int mimePos = cursor.getColumnIndex(ContactsContract.Data.MIMETYPE);
-        final int lookupPos = cursor.getColumnIndex(ContactsContract.Data.LOOKUP_KEY);
-        //final int photoPos = cursor.getColumnIndex(ContactsContract.Data.PHOTO_URI);
-
-        final int CidPos = cursor.getColumnIndex(ContactsContract.Contacts._ID);
-        //final int CnamePos = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+        /*String selection = ContactsContract.Data.HAS_PHONE_NUMBER+"=1 AND (("+
+                ContactsContract.Data.MIMETYPE+"='"+GROUPMEMBERSHIP+"' AND "+
+                ContactsContract.Data.DATA1+"=?) OR "+
+                ContactsContract.Data.MIMETYPE+"='"+PHOTO+"')";*/
+        String selection = ContactsContract.Data.HAS_PHONE_NUMBER+"=1 AND "+
+                ContactsContract.Data.MIMETYPE+"='"+GROUPMEMBERSHIP+"' AND "+
+                ContactsContract.Data.DATA1+"=?";
+        Cursor cursor = cr.query(data, clms, selection, selectionArgs, null);
 
         if (cursor != null) {
+
+            final int idPos = cursor.getColumnIndex(ContactsContract.Data.CONTACT_ID);
+            final int namePos = cursor.getColumnIndex(ContactsContract.Data.DISPLAY_NAME);
+            final int photoURIPos = cursor.getColumnIndex(ContactsContract.Data.PHOTO_URI);
+
             if(cursor.getCount() < 1) {
                 Pair<Long, String> temp = new Pair<Long, String>(0L, "Something went Wrong: Found no Contacts in Group "+ title);
                 people.add(temp);
             }
             else{
                 while(cursor.moveToNext()) {
-                    String name = cursor.getString(namePos);
                     long ID = cursor.getLong(idPos);
-                    String lookup = cursor.getString(lookupPos);
+                    String name = cursor.getString(namePos);
+
                     Log.d(TAG+"Contacts", "NAME = "+ name);
                     Log.d(TAG+"Contacts", "CONTACT_ID = "+ID);
-                    Log.d(TAG+"Contacts", "_ID = "+cursor.getString(CidPos));
-                    Log.d(TAG+"Contacts", "LOOKUP_KEY = "+lookup);
-                    Pair<Long, String> temp = new Pair<Long, String>(ID, name);
+                    Log.d(TAG+"ContactsPhoto", "PHOTO_URI = "+cursor.getString(photoURIPos));
+
+                    Pair<Long, String> temp = new Pair<>(ID, name);
                     people.add(temp);
+                    if(cursor.getString(photoURIPos) != null) {  ContactPhotos.put(ID, cursor.getString(photoURIPos));}
                 }
             }
         }
@@ -198,57 +189,133 @@ public class Presenter {
         return people;
     }
 
+    HashMap getPhotos(){
+        final String tag = "getPhoto";
+        HashMap<Long, Bitmap> map = new HashMap();
+        Set<Long> keys = ContactPhotos.keySet();
+        Log.d(TAG+tag, "getPhoto() with set "+ keys.toString());
+        String selection = " AND "+ContactsContract.Data.MIMETYPE+" = "+ContactsContract.CommonDataKinds.Photo.PHOTO;
+        String[] projection = {ContactsContract.CommonDataKinds.Photo.PHOTO};
+        for(Long item:keys){
+            Cursor cursor = getCursor(item, null, "");
+            final int index = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Photo.PHOTO);
+            final int mimeType = cursor.getColumnIndex(ContactsContract.Data.MIMETYPE);
+
+            if(cursor.moveToFirst()) {
+                Log.d(TAG + tag, "Cursor_position_move = " + cursor.getPosition());
+                Log.d(TAG + tag, "mimeType = " + cursor.getString(mimeType));
+                Log.d(TAG + tag, "index_name = " + cursor.getColumnName(index));
+                byte[] bytes = cursor.getBlob(index);
+                Bitmap bytesMap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                map.put(item, bytesMap);
+            }
+        }
+        return map;
+    }
+
     /**
      * addConversation creates a new sms or mms object to be sent
-     * @param persons the numbers of the participants
+     * @param people the numbers of the participants
      */
-    public void addConversation(List<String> persons) {
+    public void addConversation(List<Pair<Long, String>> people) {
+        List<String> phonNums = new ArrayList<>();
+        final int main = ContactsContract.CommonDataKinds.Phone.TYPE_MAIN;
+        final int mobile = ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE;
+        final int other = ContactsContract.CommonDataKinds.Phone.TYPE_OTHER;
+        final int custom = ContactsContract.CommonDataKinds.Phone.TYPE_CUSTOM;
+        final int wmobile = ContactsContract.CommonDataKinds.Phone.TYPE_WORK_MOBILE;
+        final int pager = ContactsContract.CommonDataKinds.Phone.TYPE_PAGER; //?
 
-
+        for(Pair person: people) {
+            Cursor c = cr.query(ContactsContract.Data.CONTENT_URI,
+                    new String[]{ContactsContract.Data._ID, ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.LABEL},
+                    ContactsContract.Data.CONTACT_ID + "=?" + " AND "
+                            + ContactsContract.Data.MIMETYPE + "='" + ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE + "'",
+                    new String[]{String.valueOf(person.first)}, null);
+            if(c != null) {
+                c.moveToFirst();
+                int type = c.getInt(c.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE));
+                Log.d(TAG+"numbers", "Type = "+type);
+                phonNums.add(c.getString(c.getColumnIndex(ContactsContract.Data.DATA1)));
+            }
+        }
+        Log.d(TAG+"numbers", "phonenumbers = "+phonNums.toString());
+        for(String num: phonNums) { send(num); }
     }
 
     /**
      * Retrieves the contact associated with a phone number
-     *
      * @param ID The ID of the contact to search for
      * @return the cursor containing all the info in the data table for this contact
      */
-    private Cursor getContactByID(long ID) {
-        String SingleSelection = ContactsContract.Data.CONTACT_ID+" = "+ID;
-        Cursor SingleCursor = cr.query(ContactsContract.Data.CONTENT_URI, null, SingleSelection, null, null);
+    private Cursor getCursor(long ID, String[] projection, String selection) {
+        String SingleSelection = ContactsContract.Data.CONTACT_ID+" = "+ID+selection;
+        Uri data = ContactsContract.Data.CONTENT_URI;
+        Cursor SingleCursor = cr.query(data, projection, SingleSelection, null, null);
 
-        int colm_count = SingleCursor.getColumnCount();
-        Log.d(TAG + "SingleCursor", "ColumCount = " + colm_count);
+        Log.d(TAG + "SingleCursor", "CursorCount = " + SingleCursor.getColumnCount());
+        for(String name:SingleCursor.getColumnNames()){
+            Log.d(TAG + "SingleCursor", "ColumNames = " +name);
+        }
+        Log.d(TAG + "SingleCursor", "ColumCount = " + SingleCursor.getCount());
+
         int display_index = SingleCursor.getColumnIndex("display_name");
         Log.d(TAG + "SingleCursor", "nameIndex = " + display_index);
-        String[] colms = SingleCursor.getColumnNames();
-        for(String colm:colms){
-            Log.d(TAG+"SingleCursor", colm);
-        }
-
+        //SingleCursor.moveToFirst();
+        //Log.d(TAG + "SingleCursor", "name = " + SingleCursor.getString(display_index));
+        /*
+        int mimeTypePos = SingleCursor.getColumnIndex(ContactsContract.Data.MIMETYPE);
         if (SingleCursor != null) {
             if (SingleCursor.getCount() > 0) {
                 while(SingleCursor.moveToNext()) {
                     String display_name = SingleCursor.getString(display_index);
                     Log.d(TAG + "SingleCursor", "Name = " + display_name);
+                    String mimeType = SingleCursor.getString(mimeTypePos);
                 }
             }
-        }
+        }*/
         return SingleCursor;
     }
-        /*ContentResolver cr = context.getContentResolver();
-        Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(name));//only for phone#s
+
+    public void testCursor(){
+        final String tag = "TestCursor:";
+        Uri Results;
+        Cursor cursor = context.getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,
+                new String[] {ContactsContract.Contacts.LOOKUP_KEY}, null, null, null);
+        if (cursor != null) {
+            try {
+                StringBuilder uriListBuilder = new StringBuilder();
+                int index = 0;
+                while (cursor.moveToNext()) {
+                    if (index != 0) uriListBuilder.append(':');
+                    uriListBuilder.append(cursor.getString(0));
+                    index++;
+                }
+                Results = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_MULTI_VCARD_URI,
+                        Uri.encode(uriListBuilder.toString()));
+                Log.d(TAG+tag, "Results String = "+ Results.toString());
+                Log.d(TAG+tag, "Results EncodedPath= "+ Results.getEncodedPath());
+                Log.d(TAG+tag, "Results Path = "+ Results.getPath());
+                Log.d(TAG+tag, "Results EncodedFrag= "+ Results.getEncodedFragment());
+                Log.d(TAG+tag, "Results PathSegments = "+Results.getPathSegments());
+            } catch (Exception e){ }
+            cursor.close();
+        }
+    }
+        /*getContactName(phonenumber){
+        ContentResolver cr = context.getContentResolver();
+        Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phonenumber));//only for phone#s
         Cursor cursor = cr.query(uri, new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME}, null, null, null);
         if (cursor == null) {
-            return Name;
+            return phonenumber;
         }
         if (cursor.moveToFirst()) {
-            Name = cursor.getString(cursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
+            phonenumber = cursor.getString(cursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
         }
         if (cursor != null && !cursor.isClosed()) {
             cursor.close();
         }
-        return Name;*/
+        return phonenumber;*/
 
     public void send(String person){
         String textToSend = " has started a conversation"; //TODO add users name
@@ -270,6 +337,10 @@ public class Presenter {
         }).start();
     }
 }
+
+
+
+
 /*
 import com.google.gson.Gson;
 import java.io.FileInputStream;
